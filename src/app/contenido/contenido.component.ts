3f07d81665bdd032b2491deff56776e7658a6e32
tommiramone <88351561+tommiramone@users.noreply.github.com>
2023-09-20 18:54:49 -0300

    Primer commit con página base

---
+++ src/app/contenido/contenido.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contenido',
+  templateUrl: './contenido.component.html',
+  styleUrls: ['./contenido.component.css'],
+})
+export class ContenidoComponent {}
---
+++ src/app/header-footer/header-footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-footer',
+  templateUrl: './header-footer.component.html',
+  styleUrls: ['./header-footer.component.css'],
+})
+export class HeaderFooterComponent {}
