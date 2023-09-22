8d7fc605775e85b0790efed4580713c21072800b
tommiramone <88351561+tommiramone@users.noreply.github.com>
2023-09-21 22:59:52 -0300

    funcion de scroll al cambiar de pagina a medio aplicar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
+  isSticky = false;
 
+  // Con hostListener puedo escuchar el evento, en este caso scroll y en este caso se llama a la funcion OnScroll
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    // Revista la posicion scrolleada y en base a eso cambia el valor de isSticky
+    if (window.scrollY > 470) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
+  }
 }
--- src/app/seccion-cuatro/seccion-cuatro.component.ts
+++ src/app/seccion-cuatro/seccion-cuatro.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-seccion-cuatro',
   templateUrl: './seccion-cuatro.component.html',
-  styleUrls: ['./seccion-cuatro.component.css']
+  styleUrls: ['./seccion-cuatro.component.css'],
 })
 export class SeccionCuatroComponent {
+  // Se inyecta el padre para poder utilizar sus metodos
+  constructor(@Inject(AppComponent) private padre: AppComponent) {}
 
+  // se crea un metodo para el hijo y se llama al metodo original del padre.
+  resetearScrollHijo(): void {
+    this.padre.resetearScrollPosicion();
+  }
 }
