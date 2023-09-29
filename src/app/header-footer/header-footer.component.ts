74d05540495a90d366c80d953e69c5c97d726a68
tommiramone <88351561+tommiramone@users.noreply.github.com>
2023-09-28 21:06:38 -0300

    se corrigio el bug de navbar en responsive y se lo personalizó

--- src/app/header-footer/header-footer.component.ts
+++ src/app/header-footer/header-footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { fadeIn } from '../../animations/fadeIN';
+import { fadeIn } from '../../animations/fadeIn';
 import { caida } from '../../animations/down';
 
 @Component({
